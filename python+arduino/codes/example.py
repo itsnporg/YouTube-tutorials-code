0c1cde48c7d562576b95f65520a025520189e6e2
ASACHIT <yadavkumarsachhit@gmail.com>
2021-07-19 23:08:13 +0545

    bug fixed in example.py

--- python+arduino/codes/example.py
+++ python+arduino/codes/example.py
@@ -4,7 +4,7 @@ import time
 board = pyfirmata.Arduino("COM5")  #arduino is connected in my laptop port name COM5, you can find your from ARDUINO IDE
 
 #blinking 10 time
-while True:
+for blink in range(10):
     board.digital[13].write(1)
     time.sleep(1)
     board.digital[13].write(0)
